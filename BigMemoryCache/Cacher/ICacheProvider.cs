97bad16137e20f9b04a634f01c18d9a8cba14e12
Abed Al Rahman Itani <Abed.Itani@hedgeguard.com>
2023-09-30 18:05:11 +0100

    Add the CachedItemEvictedEvent to the interface

--- BigMemoryCache/Cacher/ICacheProvider.cs
+++ BigMemoryCache/Cacher/ICacheProvider.cs
@@ -1,7 +1,11 @@
-﻿namespace Cacher;
+﻿using Cacher.EventHandlers;
+
+namespace Cacher;
 
 public interface ICacheProvider
 {
+    event CachedItemEvictedEventHandler? CachedItemEvictedEvent;
+
     object? Get(string key);
 
     void Add(string key, object item);
